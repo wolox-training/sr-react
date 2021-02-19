1aff4501ab2321857396013f47d84956b0a85050
santiagoRD <santi_diaz17@hotmail.com>
2021-02-19 16:04:35 -0500

    feature(signup): create file to test signup form

---
+++ src/screens/SignUp/signUp.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import SignUp from './index';
+
+describe('Testing SignUp Component', () => {
+  it('', () => {
+    render(<SignUp />);
+  });
+});
