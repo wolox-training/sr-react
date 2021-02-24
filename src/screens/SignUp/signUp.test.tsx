15f814deba535780f67e7560896e1584aac9f2f2
santiagoRD <santi_diaz17@hotmail.com>
2021-02-24 18:09:54 -0500

    modify test according to comments

--- src/screens/SignUp/signUp.test.tsx
+++ src/screens/SignUp/signUp.test.tsx
@@ -1,51 +1,48 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
 import { RESPONSE_STATUS } from 'constants/index';
+import { mockSignUpResponse } from 'mocks';
 
 import SignUp from './index';
 
+const server = setupServer();
+rest.post('/users', (_, res, ctx) => res(ctx.status(RESPONSE_STATUS.ok), ctx.json(mockSignUpResponse)));
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => {
+  server.close();
+  cleanup();
+});
 describe('Testing SignUp Component', () => {
-  const server = setupServer();
-  rest.post(`${process.env.REACT_APP_BASE_URL}/users`, (req, res, ctx) =>
-    res(ctx.status(RESPONSE_STATUS.ok), ctx.json({ ok: true }))
-  );
   beforeEach(() => {
     render(<SignUp />);
   });
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
   test('Should validate empty fields onSubmit', async () => {
-    const allFieldsEmpty = 5;
-    userEvent.click(screen.getByRole('textbox', { name: /SignUp:email/ }));
-    expect(await screen.findAllByRole('alert')).toHaveLength(allFieldsEmpty);
+    const allEmptyFields = 5;
+    userEvent.click(screen.getByRole('button', { name: /signup/i }));
+    expect(await screen.findAllByRole('alert')).toHaveLength(allEmptyFields);
   });
 
-  test('Should display error when email value is invalid', async () => {
+  test('Should display error in onBlur and email format is not valid', async () => {
     userEvent.type(screen.getByLabelText(/SignUp:email/i), 'invalid-email');
     userEvent.click(screen.getByRole('textbox', { name: /SignUp:email/ }));
-    await waitFor(() => {
-      expect((screen.getByLabelText(/SignUp:email/i) as HTMLInputElement).value).toBe('invalid-email');
-    });
+    await waitFor(() => expect(screen.getByLabelText(/SignUp:email/i)).toBeInTheDocument());
   });
 
   test('should can`t submit form having at least one error', async () => {
-    const onSubmit = jest.fn();
     userEvent.type(screen.getByLabelText(/SignUp:email/i), 'invalid-email');
     userEvent.click(screen.getByRole('textbox', { name: /SignUp:email/ }));
     userEvent.click(screen.getByRole('button', { name: /signup/i }));
-    await waitFor(() => {
-      expect(onSubmit).not.toBeCalled();
-    });
+    await waitFor(() => expect(screen.getByLabelText(/SignUp:email/i)).toBeInTheDocument());
   });
 
   test('should call onSubmit function after click submit button with all fields full', async () => {
     server.use(
-      rest.post(`${process.env.REACT_APP_BASE_URL}/users`, (req, res, ctx) =>
+      rest.post(`${process.env.REACT_APP_BASE_URL}/users`, (_, res, ctx) =>
         res(ctx.status(RESPONSE_STATUS.created), ctx.json({ ok: true }))
       )
     );
